36950dc204943e4bbc69cfadff0cdd28bb5693d4
viniciusucanutx <viniciuscanutmz@gmail.com>
2025-02-12 15:43:17 -0300

    :bug: fix: Adicionado certificado TLS.

--- webfilmsapi/config/db.py
+++ webfilmsapi/config/db.py
@@ -5,9 +5,11 @@ import certifi
 
 load_dotenv()
 
+ca = certifi.where()
+
 MONGO_DB_ACCESS = os.getenv("MONGO_DB_ACCESS")
 
-conn = MongoClient(MONGO_DB_ACCESS)
+conn = MongoClient(MONGO_DB_ACCESS, tlsCAFile=ca)
 
 db = conn.movies
 dbc = conn.channels
